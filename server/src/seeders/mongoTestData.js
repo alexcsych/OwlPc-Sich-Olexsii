8461551baaddff36d95db42b00421e5f72d078dd
alexcsych <alexsejsi4@gmail.com>
2023-09-23 18:59:05 +0300

    Update mongoTestData

---
+++ server/src/seeders/mongoTestData.js
@@ -0,0 +1,89 @@
+const { User, Product, Order } = require('../models');
+const userData = require('./User.json');
+const productData = require('./Product.json');
+
+async function createTestData () {
+  try {
+    const users = await User.create(userData);
+    const products = await Product.create(productData);
+
+    const orders = await Order.create([
+      {
+        user: users[0]._id,
+        products: [
+          { product: products[0]._id, quantity: 2 },
+          { product: products[1]._id, quantity: 1 },
+        ],
+      },
+      {
+        user: users[1]._id,
+        products: [
+          { product: products[1]._id, quantity: 2 },
+          { product: products[2]._id, quantity: 1 },
+        ],
+      },
+    ]);
+    await Promise.all(
+      orders.map(async order => {
+        await order.calculateTotalPrice();
+        await order.save();
+      })
+    );
+
+    // const updateQuery = { _id: orders[0]._id };
+    // const updateData = { $set: { 'products.$[].quantity': 5 } };
+
+    // const updatedOrder = await Order.findOneAndUpdate(updateQuery, updateData, {
+    //   new: true,
+    // });
+
+    // await updatedOrder.calculateTotalPrice();
+    // await updatedOrder.save();
+  } catch (error) {
+    console.error('Error creating test data:', error);
+  }
+}
+
+createTestData();
+
+// const createOrder = async orderData => {
+//   try {
+//     const order = await Order.create(orderData);
+//     await order.calculateTotalPrice();
+//     return order;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// const updateOrder = async (orderId, updateData) => {
+//   try {
+//     const order = await Order.findById(orderId);
+//     if (!order) {
+//       throw new Error('Заказ не найден');
+//     }
+
+//     Object.assign(order, updateData);
+//     await order.calculateTotalPrice();
+//     return order;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// const newOrderData = {
+//   user: '650efbbc4982bec5a4853ee4',
+//   products: [
+//     { product: '650efbbc4982bec5a4853eea', quantity: 2 },
+//     { product: '650efbbc4982bec5a4853eeb', quantity: 1 },
+//   ],
+// };
+
+// createOrder(newOrderData);
+
+// const orderIdToUpdate = '650efbbc4982bec5a4853ef3';
+// const updateData = {
+//   products: [{ product: '650efbbc4982bec5a4853eec', quantity: 2 }],
+// };
+
+// updateOrder(orderIdToUpdate, updateData);
