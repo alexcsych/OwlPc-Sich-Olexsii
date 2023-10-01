034a202835ef68c0ef7d09e1d926cad3518d068e
alexcsych <alexsejsi4@gmail.com>
2023-10-01 23:09:19 +0300

    Add router validators signup and Add router login

--- server/src/router/usersRouter.js
+++ server/src/router/usersRouter.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const usersRouter = Router();
 const { usersController } = require('./../controllers');
+const { validators } = require('../middleware');
 
 usersRouter
   .route('/signup')
-  // .get(usersController.getUsers)
-  .post(usersController.createUser);
+  .post(validators.validateSignUpData, usersController.createUser);
+
+usersRouter
+  .route('/login')
+  .post(validators.validateLogInData, usersController.loginUser);
 
 module.exports = usersRouter;
