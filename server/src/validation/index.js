b704b573cd19fe47bdf19dbf19458e244c23d756
alexcsych <alexsejsi4@gmail.com>
2023-10-01 23:06:32 +0300

    Add server validation

---
+++ server/src/validation/index.js
@@ -0,0 +1,48 @@
+const yup = require('yup');
+const { User } = require('../models');
+
+module.exports.signUpSchem = yup.object().shape({
+  name: yup.string().required('Name обязательно для заполнения'),
+  email: yup
+    .string()
+    .email('Неверный формат email')
+    .test(
+      'is-unique',
+      'Этот email уже зарегистрирован',
+      async function (value) {
+        const user = await User.findOne({ email: value });
+        return !user;
+      }
+    )
+    .required('Email обязателен для заполнения'),
+  password: yup
+    .string()
+    .min(6, 'Password must be 6 characters long')
+    .matches(/[0-9]/, 'Password requires a number')
+    .matches(/[a-z]/, 'Password requires a lowercase letter')
+    .matches(/[A-Z]/, 'Password requires an uppercase letter')
+    .matches(/[^\w]/, 'Password requires a symbol')
+    .required('Password обязателен для заполнения'),
+  role: yup
+    .string()
+    .oneOf(
+      ['customer', 'creator', 'moderator'],
+      'Role must be one of the following values: customer, creator, moderator'
+    )
+    .required('Role обязателен для заполнения'),
+});
+
+module.exports.logInSchem = yup.object().shape({
+  email: yup
+    .string()
+    .email('Неверный формат email')
+    .required('Email обязателен для заполнения'),
+  password: yup
+    .string()
+    .min(6, 'Password must be 6 characters long')
+    .matches(/[0-9]/, 'Password requires a number')
+    .matches(/[a-z]/, 'Password requires a lowercase letter')
+    .matches(/[A-Z]/, 'Password requires an uppercase letter')
+    .matches(/[^\w]/, 'Password requires a symbol')
+    .required('Password обязателен для заполнения'),
+});
