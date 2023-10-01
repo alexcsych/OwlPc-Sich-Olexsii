caa2f3bf083efc752dd6412cbc2f86ff28e1ae44
alexcsych <alexsejsi4@gmail.com>
2023-10-01 23:09:57 +0300

    Update usersController and Add loginUser

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const createHttpError = require('http-errors');
 const { User } = require('./../models');
+
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS, 10);
 
 module.exports.createUser = async (req, res, next) => {
   const { body } = req;
+  console.log('createUser');
   try {
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash(body.password, salt);
+    body.password = hashedPassword;
     const createdUser = await User.create(body);
 
     if (!createdUser) {
@@ -15,11 +24,27 @@ module.exports.createUser = async (req, res, next) => {
   }
 };
 
-module.exports.getUsers = async (req, res, next) => {
-  const { limit = 10 } = req.query;
+module.exports.loginUser = async (req, res, next) => {
+  const { body } = req;
+  console.log('loginUser');
+  console.log('body :>> ', body);
 
   try {
-    const foundUser = await User.find().sort({ _id: 1 }).limit(limit).skip(0);
+    const foundUser = await User.findOne({ email: body.email });
+
+    if (!foundUser) {
+      return next(createHttpError(404, 'User Not Found'));
+    }
+    console.log('body.password :>> ', body.password);
+    console.log('foundUser.password :>> ', foundUser.password);
+    const isPasswordValid = await bcrypt.compare(
+      body.password,
+      foundUser.password
+    );
+
+    if (!isPasswordValid) {
+      return next(createHttpError(401, 'Invalid Password'));
+    }
 
     res.status(200).send({ data: foundUser });
   } catch (err) {
