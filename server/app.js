d10e8e81584939c998bd197a3f1943da5a1fb507
alexcsych <alexsejsi4@gmail.com>
2023-10-08 18:46:27 +0300

    Add swagger

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const { errorHandlers } = require('./src/middleware');
 const router = require('./src/router');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
 app.use(express.json());
-
 app.use('/api', router);
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorHandlers.errorHandler);
 
 module.exports = app;
